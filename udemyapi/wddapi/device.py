cae2dd3781afb87cc766acdabc30f58ce272f6d2
Sri Balaji M <srisvit@gmail.com>
2024-05-21 05:58:27 -0400

    added data validation - schema, path, query parameter DV

--- udemyapi/wddapi/device.py
+++ udemyapi/wddapi/device.py
@@ -1,16 +1,16 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Path, Query, HTTPException
 from datetime import datetime
-from schema import Device
+from schema import Device, DeviceRequest
 import uuid
 import uvicorn
 
 app = FastAPI()
 
 DEVICES = [
-    Device(device_id=2327, house_id=65, run_id=str(uuid.uuid4()), temperature=16.04, humidity=12.12,
+    Device(device_id=2327, house_id=65, run_id=str(uuid.uuid4()), temperature=16.04, humidity=21.12,
            washer_vib_id=32, dryer_vib_id=32, motion_detected=True, smoke_detected=False,
            load_dt=str(datetime.now()), created_at=str(datetime.now()), updated_at=str(datetime.now())),
-    Device(device_id=1931, house_id=35, run_id=str(uuid.uuid4()), temperature=17.04, humidity=15.12,
+    Device(device_id=1931, house_id=35, run_id=str(uuid.uuid4()), temperature=17.04, humidity=55.12,
            washer_vib_id=35, dryer_vib_id=312, motion_detected=False, smoke_detected=False,
            load_dt=str(datetime.now()), created_at=str(datetime.now()), updated_at=str(datetime.now()))
 ]
@@ -29,11 +29,16 @@ async def device(device_id: int):
     """
     return the result of a specific device
     """
-    return list(filter(lambda x: x.device_id == device_id, DEVICES))
+    device_ls =  list(filter(lambda x: x.device_id == device_id, DEVICES))
+
+    if device_ls is not None and len(device_ls) > 0:
+        return device_ls
+    else:
+        raise HTTPException(status_code=404, detail="Device not found")
 
 
 @app.get("/device/{device_id}")
-async def temperature_threshold(device_id: int, temperature: float = None):
+async def temperature_threshold(device_id: int = Path(gt = 0), temperature: float = Query(gt=0, lt=100)):
     """
     return the result of a specific device filtered by temperature threshold
     """
@@ -53,8 +58,44 @@ async def run_status(run_id: str):
 
     return list(filter(lambda x: x.run_id == run_id, DEVICES))
 
+@app.get("/all_devices")
+async def get_devices():
+    """
+    return all the devices list in the system
+    """
 
-# @app.post("/device/create_device")
-# async def create_device(new_device: Device):
-#     DEVICES.append(new_device)
-#     return True
+    return list(filter(lambda x: x.__dict__, DEVICES))
+
+@app.post("/device/create_device")
+async def create_device(new_device: DeviceRequest):
+    new_device = Device(**new_device.dict())
+    DEVICES.append(create_device_id(new_device))
+    return True
+
+@app.put("/device/update_device")
+async def update_device(update_device: DeviceRequest):
+    check_flag = False
+    for i,d in enumerate(DEVICES):
+        if d.device_id == update_device.device_id:
+            DEVICES[i] = update_device
+            check_flag = True
+    if check_flag == False:
+        raise HTTPException(status_code=404, detail="Device not found to update")
+
+    return True
+
+@app.delete("/device/delete_device")
+async def delete_device(delete_device_id: int = Query(gt=0)): #path gives extra validation to path parameters
+    check_flag = False
+    for i,d in enumerate(DEVICES):
+        if d.device_id == delete_device_id:
+            DEVICES.pop(i)
+            check_flag = True
+    if check_flag == False:
+        raise HTTPException(status_code=404, detail="Device not found to delete")
+
+    return True
+
+def create_device_id(device: DeviceRequest):
+    device.device_id =1 if(len(DEVICES) == 0) else DEVICES[-1].device_id + 1
+    return device
--- udemyapi/wddapi/schema.py
+++ udemyapi/wddapi/schema.py
@@ -1,21 +1,74 @@
 """
+author: "Sri Balaji M"
 This module contains all the pydantic models used in the project
 """
 
 from pydantic import BaseModel, Field
 from typing import List, Optional
 
+# Field are used for data validation to a particular filed in the model(table). For each type we have many data validation methods
 
-class Device(BaseModel):
+# TA1: Create constaints for allt eh necessary fields
+# TA2: Fill the optional value to the appropriate fields
+# TA3: Convert the date columns into actual Pydantic datetime column
+
+
+class Device():
     device_id: int
     house_id: int
     run_id: str
-    temperature: float = Field(gt=-100.0, lt=100.0)
+    temperature: float
     humidity: float
-    washer_vib_id: Optional[int]
-    dryer_vib_id: Optional[int]
+    washer_vib_id: int
+    dryer_vib_id: int
     motion_detected: bool
     smoke_detected: bool
     load_dt: str
     created_at: str
     updated_at: str
+
+    def __init__(self, device_id, house_id, run_id, temperature, humidity, washer_vib_id,dryer_vib_id,motion_detected,smoke_detected,load_dt, created_at, updated_at):
+        self.device_id = device_id
+        self.house_id = house_id
+        self.run_id = run_id
+        self.temperature = temperature
+        self.humidity = humidity
+        self.washer_vib_id = washer_vib_id
+        self.dryer_vib_id = dryer_vib_id
+        self.motion_detected = motion_detected
+        self.smoke_detected = smoke_detected
+        self.load_dt = load_dt
+        self.created_at = created_at
+        self.updated_at = updated_at
+
+class DeviceRequest(BaseModel):
+    device_id: Optional[int] = Field(title = "device id will be created on its own")
+    house_id: int
+    run_id: str
+    temperature: float = Field(gt=-100.0, lt=100.0)
+    humidity: float = Field(gt=20.0, lt=80.0)
+    washer_vib_id: Optional[int] = None
+    dryer_vib_id: Optional[int] = None
+    motion_detected: bool
+    smoke_detected: bool
+    load_dt: str
+    created_at: str
+    updated_at: str
+
+    # class Config:
+    #     schema_extra = {
+    #         'example': {
+    #             'device_id': "device id will be created on its own",
+    #             'house_id': "house where the device is located",
+    #             'run_id': "run_id of the particular run of that device",
+    #             'temperature': "temperature from the DHT11 sensor",
+    #             'humidity': "humidity from the DHT11 sensor",
+    #             'washer_vib_id': "washer vib id of the device",
+    #             'dryer_vib_id': "dryer vib id of the device",
+    #             'motion_detected': "motion sensor value from the HC-SR501",
+    #             'smoke_detected': "smoke sensor value from the MQ-135",
+    #             'load_dt': "load timestamp of the run",
+    #             'created_at': "created datetime of the record",
+    #             'updated_at': "updated datetime of the record"
+    #         }
+    #     }
